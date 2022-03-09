f7c164291941abd2030f0b143a45d137511c0e93
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-03-09 17:05:40 +1100

    added .gitignore, connection.js, and questions

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const sql = require('mysql2');
 const inquirer = require('inquirer');
+require('dotenv').config();
+const db = require('./util/connection');
 
-const promptUser = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'list',
             message: 'What would you like to do?',
             name: 'firstPrompt',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'End Session'
+            ]
         }
-    ])
-}
-
-promptUser();
-
+    ];
+    
